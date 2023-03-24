ec6735e2179042f5b26bde4baacddb509bab4d8e
Christopher Tracey <christopher_tracey@natureserve.org>
2023-03-24 19:25:06 +0000

    change from EVT code to system name for files

--- FWS_SE_Condition_NEW.py
+++ FWS_SE_Condition_NEW.py
@@ -1,4 +1,4 @@
-import arcpy, os, shutil
+import arcpy, os, re
 from arcpy.sa import *
 
 # Check out any necessary licenses.
@@ -25,41 +25,48 @@ dataFireDep = r"S:\Data\External\LANDFIRE_Fire_Departure\LF2020_VDep_220_CONUS\L
 templateInvasiveSource = r"S:\Projects\USFWS\SE_FWS_Habitat_2022\FWS_HabConScriptTesting\fwshabcondition\template_InvasiveSource.lyrx"
 templateLCM = r"S:\Projects\USFWS\SE_FWS_Habitat_2022\FWS_HabConScriptTesting\fwshabcondition\template_LCM.lyrx"
 
-#path = r"S:\Projects\_Workspaces\Jordana_Anderson\SE_USFWS\FWS_SE_Condition\FWS_SE_Condition_testing\FWS_SE_Condition_testing.gdb\combined_EVT_Clip"
-value_list = []   
-
 outExtractByMask = ExtractByMask(combined_EVT_tif, hexclip)  # THIS WILL LIKELY HAVE TO BE MODIFED FOR THE FULL SCRIPT!!!!
 outExtractByMask1 = ExtractByAttributes(outExtractByMask, '"Value_1" <> 0') 
 outExtractByMask1.save("EVTclip.tif")
 
-with arcpy.da.SearchCursor("EVTclip.tif", ["Value"]) as cursor:
+value_list = [] 
+with arcpy.da.SearchCursor("EVTclip.tif", ["Value","NatureServ"]) as cursor:
     for row in cursor:
-        value_list.append(row[0])
-print(value_list)
+        value_list.append(row)
+        
+print("Creating condition datasets for " + str(len(value_list)) + " ecological systems.")
+print("===============================================================================")
 
 for index in value_list:
-    columnValue = index
-    print("working on EVT" + str(columnValue))
+    EVTvalue = index[0]
+    EVTname = index[1]
+    # create a clean version of the system name filenames
+    pattern = re.compile('[^a-zA-Z0-9\s]+')
+    EVTclean = re.sub(pattern, '', EVTname)
+    pattern = re.compile('\s+')
+    EVTclean = re.sub(pattern, '', EVTclean)
     
+    print("working on " + EVTname + " (EVT " + str(EVTvalue) + ")")
+        
     # Process: Extract by Attributes (Extract by Attributes) (sa)
     print("- working on extracting the EVT data")
-    nameEVT = "tmp_Extract_" + str(columnValue) + ".tif"
-    w_clause = '"VALUE" = ' + "%s" %columnValue
+    nameEVT = "tmp_Extract_" + str(EVTvalue) + ".tif"
+    w_clause = '"VALUE" = ' + "%s" %EVTvalue
     tmp_Extract_Value_tif = ExtractByAttributes(in_raster="EVTclip.tif", where_clause=w_clause)
     tmp_Extract_Value_tif.save(os.path.join(arcpy.env.workspace, nameEVT))
 
     # Select out the hexagons that overlap the EVT cells
     print("- intersecting the EVT layer with the hex grid")
-    tmp_Value_raster2pt = fr"tmp_{columnValue}_raster2pt"
+    tmp_Value_raster2pt = fr"tmp_{EVTvalue}_raster2pt"
     arcpy.conversion.RasterToPoint(in_raster=tmp_Extract_Value_tif, out_point_features=tmp_Value_raster2pt, raster_field="VALUE")    
     hexgridselection = arcpy.management.SelectLayerByLocation(in_layer=[hexgrid], overlap_type="INTERSECT", select_features=tmp_Value_raster2pt, search_distance="", selection_type="NEW_SELECTION", invert_spatial_relationship="NOT_INVERT")
-    hexgridselection = arcpy.conversion.FeatureClassToFeatureClass(in_features=hexgridselection, out_path=os.path.join(arcpy.env.workspace,HabitatCondition_gdb), out_name=f"hex100ac_{columnValue}", where_clause="")
+    hexgridselection = arcpy.conversion.FeatureClassToFeatureClass(in_features=hexgridselection, out_path=os.path.join(arcpy.env.workspace,HabitatCondition_gdb), out_name=f"hex100ac_{EVTclean}", where_clause="")
 
     # Work on the LCM
     print("- calculating and summarizing the LCM values")
-    tmp_Value_LCM_tif = fr"tmp_{columnValue}_LCM.tif"
+    tmp_Value_LCM_tif = fr"tmp_{EVTvalue}_LCM.tif"
     tmp_Value_LCM_tif = arcpy.sa.ExtractByMask(in_raster=dataLCM, in_mask_data=nameEVT)
-    ZonalSt_Value_LCM = fr"ZonalSt_{columnValue}_LCM"
+    ZonalSt_Value_LCM = fr"ZonalSt_{EVTvalue}_LCM"
     arcpy.sa.ZonalStatisticsAsTable(in_zone_data=hexgridselection, zone_field="GRID_ID", in_value_raster=tmp_Value_LCM_tif,
                                     out_table=ZonalSt_Value_LCM, ignore_nodata="DATA", statistics_type="MEAN")
     arcpy.management.JoinField(in_data=hexgridselection, in_field="GRID_ID", join_table=ZonalSt_Value_LCM, join_field="GRID_ID", fields=["MEAN"])[0]
@@ -69,9 +76,9 @@ for index in value_list:
 
     # Work on Invasive Risk score
     print("- calculating the invasive risk score")
-    tmp_InvRisk_tif = fr"tmp_{columnValue}_Inv.tif"
+    tmp_InvRisk_tif = fr"tmp_{EVTvalue}_Inv.tif"
     tmp_InvRisk_tif = arcpy.sa.ExtractByMask(in_raster=dataRuderal, in_mask_data=nameEVT)
-    ZonalSt_Value_InvRisk = fr"ZonalSt_{columnValue}_InvRisk"
+    ZonalSt_Value_InvRisk = fr"ZonalSt_{EVTvalue}_InvRisk"
     arcpy.sa.ZonalStatisticsAsTable(in_zone_data=hexgridselection, zone_field="GRID_ID", in_value_raster=tmp_InvRisk_tif,
                                     out_table=ZonalSt_Value_InvRisk, ignore_nodata="DATA", statistics_type="MEAN")
     arcpy.management.JoinField(in_data=hexgridselection, in_field="GRID_ID", join_table=ZonalSt_Value_InvRisk, join_field="GRID_ID", fields=["MEAN"])[0]
@@ -86,9 +93,9 @@ for index in value_list:
     
     # Work on fire departure score
     print("- calculating the fire departure score")
-    tmp_FireDep_tif = fr"tmp_{columnValue}_Inv.tif"
+    tmp_FireDep_tif = fr"tmp_{EVTvalue}_Inv.tif"
     tmp_FireDep_tif = arcpy.sa.ExtractByMask(in_raster=dataFireDep, in_mask_data=nameEVT)
-    ZonalSt_Value_FireDep = fr"ZonalSt_{columnValue}_InvRisk"
+    ZonalSt_Value_FireDep = fr"ZonalSt_{EVTvalue}_InvRisk"
     arcpy.sa.ZonalStatisticsAsTable(in_zone_data=hexgridselection, zone_field="GRID_ID", in_value_raster=tmp_FireDep_tif,
                                     out_table=ZonalSt_Value_FireDep, ignore_nodata="DATA", statistics_type="MEAN")
     arcpy.management.JoinField(in_data=hexgridselection, in_field="GRID_ID", join_table=ZonalSt_Value_FireDep, join_field="GRID_ID", fields=["MEAN"])[0]
@@ -108,4 +115,4 @@ for index in value_list:
     arcpy.management.Delete(in_data=[tmp_Value_raster2pt])   
 
 # higher level cleanup
-arcpy.management.Delete(scratchWorkspace)
+#arcpy.management.Delete(scratchWorkspace)
